8589ef7f0db80dc1488f948bd14afc310e5d246e
Shimomura Shunsuke <shunsuke0406.1246.rex@gmail.com>
2024-05-27 11:04:33 +0900

    cargo fmt

--- kble-tcp/src/main.rs
+++ kble-tcp/src/main.rs
@@ -1,13 +1,13 @@
-use std::net::{Ipv4Addr,IpAddr};
+use clap::Parser;
+use futures_util::{SinkExt, StreamExt};
+use std::net::{IpAddr, Ipv4Addr};
 use std::process::exit;
-use tokio::sync::Mutex;
+use std::sync::Arc;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio::sync::Mutex;
 use tokio_tungstenite::accept_async;
-use futures_util::{StreamExt, SinkExt};
-use std::sync::Arc;
-use clap::Parser;
+use tokio_tungstenite::tungstenite::protocol::Message;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -27,25 +27,31 @@ async fn main() -> io::Result<()> {
     let args = Args::parse();
 
     // WebSocketサーバーのリスナーを設定
-    let ws_listener = TcpListener::bind(format!("{}:{}",args.ws_addr,args.ws_port)).await?;
-    println!("WebSocket server listening on ws://{}:{}",args.ws_addr,args.ws_port);
+    let ws_listener = TcpListener::bind(format!("{}:{}", args.ws_addr, args.ws_port)).await?;
+    println!(
+        "WebSocket server listening on ws://{}:{}",
+        args.ws_addr, args.ws_port
+    );
 
     // 接続状態を追跡するためのArc<Mutex<>>を作成
     let client_connected = Arc::new(Mutex::new(false));
 
     // TCPサーバーに接続する
-    let tcp_stream = TcpStream::connect(format!("{}:{}",args.tcp_addr,args.tcp_port)).await?;
+    let tcp_stream = TcpStream::connect(format!("{}:{}", args.tcp_addr, args.tcp_port)).await?;
     let (tcp_upstream, tcp_downstream) = tokio::io::split(tcp_stream);
     let tcp_upstream = Arc::new(Mutex::new(tcp_upstream));
     let tcp_downstream = Arc::new(Mutex::new(tcp_downstream));
 
     loop {
-        let (stream, _) = ws_listener.accept().await.expect("Failed to accept connection");
+        let (stream, _) = ws_listener
+            .accept()
+            .await
+            .expect("Failed to accept connection");
 
         let client_connected = Arc::clone(&client_connected);
         let tcp_upstream = Arc::clone(&tcp_upstream);
         let tcp_downstream = Arc::clone(&tcp_downstream);
-        
+
         tokio::spawn(async move {
             let mut is_client_connected = client_connected.lock().await;
             if *is_client_connected {
@@ -53,14 +59,27 @@ async fn main() -> io::Result<()> {
             } else {
                 *is_client_connected = true;
                 drop(is_client_connected); // Mutexのロックを解除
-                handle_ws_client(stream, Arc::clone(&client_connected), Arc::clone(&tcp_upstream), Arc::clone(&tcp_downstream)).await;
+                handle_ws_client(
+                    stream,
+                    Arc::clone(&client_connected),
+                    Arc::clone(&tcp_upstream),
+                    Arc::clone(&tcp_downstream),
+                )
+                .await;
             }
         });
     }
 }
 
-async fn handle_ws_client(ws_stream: TcpStream, client_connected: Arc<Mutex<bool>>, tcp_upstream: Arc<Mutex<tokio::io::ReadHalf<TcpStream>>>, tcp_downstream: Arc<Mutex<tokio::io::WriteHalf<TcpStream>>>) {
-    let ws_stream = accept_async(ws_stream).await.expect("Error during WebSocket handshake");
+async fn handle_ws_client(
+    ws_stream: TcpStream,
+    client_connected: Arc<Mutex<bool>>,
+    tcp_upstream: Arc<Mutex<tokio::io::ReadHalf<TcpStream>>>,
+    tcp_downstream: Arc<Mutex<tokio::io::WriteHalf<TcpStream>>>,
+) {
+    let ws_stream = accept_async(ws_stream)
+        .await
+        .expect("Error during WebSocket handshake");
     println!("WebSocket client connected");
 
     let (mut ws_write, mut ws_read) = ws_stream.split();
@@ -105,7 +124,11 @@ async fn handle_ws_client(ws_stream: TcpStream, client_connected: Arc<Mutex<bool
                     exit(0);
                 }
                 Ok(n) => {
-                    if ws_write.send(Message::binary(buffer[..n].to_vec())).await.is_err() {
+                    if ws_write
+                        .send(Message::binary(buffer[..n].to_vec()))
+                        .await
+                        .is_err()
+                    {
                         println!("Failed to send message to WebSocket client");
                         break;
                     }
